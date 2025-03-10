bc33f756865d0011a9bdace3e443cf96cad17b59
Patrik Krizek <patrik.getsetuk@gmail.com>
2025-03-10 09:18:30 +0000

    Week 09

---
+++ week-09/script-intro.js
@@ -0,0 +1,12 @@
+// Variables
+var firstName = "Patrick";
+let lastName = "Krizek";
+const numberPi = 3.14;
+
+console.log(firstName);
+console.log(firstName + " " + lastName);
+
+console.log(6 + 8);
+
+let someNumbers = 5 + 6 + 8;
+console.log(someNumbers);
